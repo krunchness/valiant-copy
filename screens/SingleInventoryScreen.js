622de2fa4edccb5fddb5610a4411d8b958a27c5c
Enjay Navarro <enjay.navarro@gmail.com>
2023-11-10 03:45:59 +0800

    fixed duplicate (#14)

--- screens/SingleInventoryScreen.js
+++ screens/SingleInventoryScreen.js
@@ -9,6 +9,7 @@ const SingleInventoryScreen = ({ route, navigation }) => {
   const [showEditDialog, setShowEditDialog] = useState(false);
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
   const [data, setData] = useState(null);
+  const [showDuplicateDialog, setShowDuplicateDialog] = useState(false);
 
   useEffect(() => {
     // Set the title of the screen based on the categoryName parameter
@@ -35,6 +36,7 @@ const SingleInventoryScreen = ({ route, navigation }) => {
 
                 results.specificationInformation = rows.item(0);
                 setData(results);
+                console.log(results);
               },
               (error) => console.error('Error fetching data from rpie_specification_information:', error)
             );
@@ -94,31 +96,101 @@ const SingleInventoryScreen = ({ route, navigation }) => {
         'UPDATE rpie_specifications SET modified_date = ?, sync_status = ? WHERE id = ?',
         [currentDate, 'deleted', data.id]
       );
-      // tx.executeSql(
-      //   'INSERT INTO rpie_specifications_trash (rpie_post_id, rpie_id) VALUES (?, ?)',
-      //   [data.rpie_post_id, data.rpie_id],
-      //   () => {
-      //     // Delete data from the rpie_specifications_information table
-      //     tx.executeSql(
-      //       'DELETE FROM rpie_specification_information WHERE rpie_specs_id = ?',
-      //       [data.id],
-      //       () => {
-      //         // Delete data from the rpie_specifications table
-      //         tx.executeSql(
-      //           'DELETE FROM rpie_specifications WHERE id = ?',
-      //           [data.id],
-      //           () => {
-      //             // Navigate back to the previous screen or perform other actions as needed
-      //             navigation.goBack();
-      //           },
-      //           (error) => console.error('Error deleting data from rpie_specifications:', error)
-      //         );
-      //       },
-      //       (error) => console.error('Error deleting data from rpie_specifications_information:', error)
-      //     );
-      //   },
-      //   (error) => console.error('Error inserting data into rpie_specifications_trash:', error)
-      // );
+    });
+  };
+
+  const duplicateBtnDialog = () => {
+    setShowDuplicateDialog(true);
+  };
+
+  const hideDuplicateDialog = () => {
+    setShowDuplicateDialog(false);
+  };
+
+  const handleConfirmDuplicateDialog = () => {
+    // Duplicate the data in the database
+    duplicateData(data);
+
+    // Close the dialog
+    setShowDuplicateDialog(false);
+  };
+
+  const duplicateData = (data) => {
+    db.transaction((tx) => {
+      // Duplicate the data in the rpie_specifications table
+      tx.executeSql(
+        'INSERT INTO rpie_specifications (rpie_post_id, rpie_id, created_date, modified_date, newly_created, sync_status, status) VALUES (?, ?, ?, ?, ?, ? , ?)',
+        [0, 'Duplicate ' + data.rpie_id, data.created_date, data.modified_date, 'true', 'duplicate-only', ''],
+        (tx, result) => {
+          // Get the inserted row id
+          const id = result.insertId;
+
+          // Duplicate the data in the rpie_specification_information table
+          tx.executeSql(
+            'INSERT INTO rpie_specification_information (rpie_specs_id, installation, facility_num_name, room_num_loc, system, subsystem, assembly_category, nomenclature, rpie_index_number, new_rpie_id, rpie_index_number_code, bar_code_number, prime_component, group_name, group_risk_factor, rpie_risk_factor, rpie_spare, capacity_unit, capacity_value, manufacturer, model, serial_number, catalog_number, life_expectancy, contractor, contract_number, contract_start_date, contract_end_date, po_number, vendor, installation_date, warranty_start_date, spec_unit, spec_value, spec_corrections, equipment_hazard, equipment_hazard_corrections, area_supported, room_supported, note_date, note_text, status, status_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+              id, 
+              data.specificationInformation.installation ? data.specificationInformation.installation : '', 
+              data.specificationInformation.facility_num_name ? data.specificationInformation.facility_num_name : '',
+              data.specificationInformation.room_num_loc ? data.specificationInformation.room_num_loc : '',
+              data.specificationInformation.system ? data.specificationInformation.system : '',
+              data.specificationInformation.subsystem ? data.specificationInformation.subsystem : '',
+              data.specificationInformation.assembly_category ? data.specificationInformation.assembly_category : '', 
+              data.specificationInformation.nomenclature ? data.specificationInformation.nomenclature : '', 
+              data.specificationInformation.rpie_index_number ? 'Duplicate ' + data.specificationInformation.rpie_index_number : '', 
+              data.specificationInformation.new_rpie_id ? 'Duplicate ' + data.specificationInformation.new_rpie_id : '',
+              data.specificationInformation.rpie_index_number_code ? data.specificationInformation.rpie_index_number_code : '',
+              data.specificationInformation.bar_code_number ? data.specificationInformation.bar_code_number : '',
+              data.specificationInformation.prime_component ? data.specificationInformation.prime_component : '',
+              data.specificationInformation.group_name ? data.specificationInformation.group_name : '',
+              data.specificationInformation.group_risk_factor ? data.specificationInformation.group_risk_factor : '',
+              data.specificationInformation.rpie_risk_factor ? data.specificationInformation.rpie_risk_factor : '',
+              data.specificationInformation.rpie_spare ? data.specificationInformation.rpie_spare : '',
+              data.specificationInformation.capacity_unit ? data.specificationInformation.capacity_unit : '',
+              data.specificationInformation.capacity_value ? data.specificationInformation.capacity_value : '',
+              data.specificationInformation.manufacturer ? data.specificationInformation.manufacturer : '',
+              data.specificationInformation.model ? data.specificationInformation.model : '',
+              data.specificationInformation.serial_number ? data.specificationInformation.serial_number : '',
+              data.specificationInformation.catalog_number ? data.specificationInformation.catalog_number : '',
+              data.specificationInformation.life_expectancy ? data.specificationInformation.life_expectancy : '',
+              data.specificationInformation.contractor ? data.specificationInformation.contractor : '',
+              data.specificationInformation.contract_number ? data.specificationInformation.contract_number : '',
+              data.specificationInformation.contract_start_date ? data.specificationInformation.contract_start_date : '',
+              data.specificationInformation.contract_end_date ? data.specificationInformation.contract_end_date : '',
+              data.specificationInformation.po_number ? data.specificationInformation.po_number : '',
+              data.specificationInformation.vendor ? data.specificationInformation.vendor : '',
+              data.specificationInformation.installation_date ? data.specificationInformation.installation_date : '',
+              data.specificationInformation.warranty_start_date ? data.specificationInformation.warranty_start_date : '',
+              data.specificationInformation.spec_unit ? data.specificationInformation.spec_unit : '',
+              data.specificationInformation.spec_value ? data.specificationInformation.spec_value : '',
+              data.specificationInformation.spec_corrections ? data.specificationInformation.spec_corrections : '',
+              data.specificationInformation.equipment_hazard ? data.specificationInformation.equipment_hazard : '',
+              data.specificationInformation.equipment_hazard_corrections ? data.specificationInformation.equipment_hazard_corrections : '',
+              data.specificationInformation.area_supported ? data.specificationInformation.area_supported : '',
+              data.specificationInformation.room_supported ? data.specificationInformation.room_supported : '',
+              data.specificationInformation.note_date ? data.specificationInformation.note_date : '',
+              data.specificationInformation.note_text ? data.specificationInformation.note_text : '',
+              data.specificationInformation.status.value ? data.specificationInformation.status.value : '',
+              data.specificationInformation.status_date ? data.specificationInformation.status_date : '',
+            ],
+            (tx, result) => {
+              // Duplicate was successful, show a success message or perform other actions as needed
+              tx.executeSql(
+                'SELECT * FROM rpie_specifications WHERE id = ?',
+                [id],
+                (_, { rows }) => {
+                  let results = rows.item(0);
+
+                  navigation.navigate('SingleInventory', { rpie: results });
+                },
+                (error) => console.error('Error fetching data from rpie_specifications:', error)
+              );
+            },
+            (error) => console.error('Error duplicating data in rpie_specification_information:', error)
+          );
+        },
+        (error) => console.error('Error duplicating data in rpie_specifications:', error)
+      );
     });
   };
 
@@ -555,7 +627,7 @@ const SingleInventoryScreen = ({ route, navigation }) => {
                   </Button>
                 </View>
                 <View style={styles.buttonContainer}>
-                  <Button textColor="#fff" mode="contained" style={styles.Btn} >
+                  <Button textColor="#fff" mode="contained" style={styles.Btn} onPress={duplicateBtnDialog}>
                     Duplicate
                   </Button>
                 </View>
@@ -566,11 +638,11 @@ const SingleInventoryScreen = ({ route, navigation }) => {
                     Delete
                   </Button>
                 </View>
-                <View style={styles.buttonContainer}>
+                {/*<View style={styles.buttonContainer}>
                   <Button textColor="#fff" mode="contained" style={styles.Btn}>
                     Print
                   </Button>
-                </View>
+                </View>*/}
               </View>
 
               <Portal>
@@ -597,6 +669,19 @@ const SingleInventoryScreen = ({ route, navigation }) => {
                   <Dialog.Actions>
                     <Button onPress={hideDeleteDialog}>Cancel</Button>
                     <Button onPress={handleConfirmDeleteDialog}>Delete</Button>
+                  </Dialog.Actions>
+                </Dialog>
+
+                <Dialog visible={showDuplicateDialog} onDismiss={hideDuplicateDialog}>
+                  <Dialog.Title>Confirm Duplicate</Dialog.Title>
+                  <Dialog.Content>
+                    <Paragraph>
+                      Are you sure you want to duplicate this RPIE Specification?
+                    </Paragraph>
+                  </Dialog.Content>
+                  <Dialog.Actions>
+                    <Button onPress={hideDuplicateDialog}>Cancel</Button>
+                    <Button onPress={handleConfirmDuplicateDialog}>Duplicate</Button>
                   </Dialog.Actions>
                 </Dialog>
               </Portal>
